cbc290628e5fb7f1886436f84a68b4ab118c646f
wavesZh <waves_max@163.com>
2019-02-22 13:39:22 +0800

    delete extra variable.

--- src/main/java/io/openmessaging/storage/dledger/DLedger.java
+++ src/main/java/io/openmessaging/storage/dledger/DLedger.java
@@ -19,7 +19,6 @@ package io.openmessaging.storage.dledger;
 
 import com.alibaba.fastjson.JSON;
 import com.beust.jcommander.JCommander;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
