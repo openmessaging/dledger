23c2c53dee88a8717adc9892e61631050de5d85b
itonyli <lt429284840@gmail.com>
2021-06-09 15:20:11 +0800

    Remove invalid code

--- src/main/java/io/openmessaging/storage/dledger/DLedger.java
+++ src/main/java/io/openmessaging/storage/dledger/DLedger.java
@@ -31,22 +31,12 @@ public class DLedger {
         DLedgerServer dLedgerServer = new DLedgerServer(dLedgerConfig);
         dLedgerServer.startup();
         logger.info("[{}] group {} start ok with config {}", dLedgerConfig.getSelfId(), dLedgerConfig.getGroup(), JSON.toJSONString(dLedgerConfig));
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            private volatile boolean hasShutdown = false;
-
-            @Override
-            public void run() {
-                synchronized (this) {
-                    logger.info("Shutdown hook was invoked");
-                    if (!this.hasShutdown) {
-                        this.hasShutdown = true;
-                        long beginTime = System.currentTimeMillis();
-                        dLedgerServer.shutdown();
-                        long consumingTimeTotal = System.currentTimeMillis() - beginTime;
-                        logger.info("Shutdown hook over, consuming total time(ms): {}", consumingTimeTotal);
-                    }
-                }
-            }
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Shutdown hook was invoked");
+            long beginTime = System.currentTimeMillis();
+            dLedgerServer.shutdown();
+            long consumingTimeTotal = System.currentTimeMillis() - beginTime;
+            logger.info("Shutdown hook over, consuming total time(ms): {}", consumingTimeTotal);
         }, "ShutdownHook"));
     }
 }
