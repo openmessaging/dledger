4deae2ec661774f80320e4db338301dc34862329
yuz10 <845238369@qq.com>
2022-03-31 00:11:56 +0800

    [ISSUE #122] fix high CPU in EntryDispatcher

--- src/main/java/io/openmessaging/storage/dledger/store/file/MmapFileList.java
+++ src/main/java/io/openmessaging/storage/dledger/store/file/MmapFileList.java
@@ -23,6 +23,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
