aa6b523c9ff216a7de5e71284023d1044badcbc4
TheR1sing3un <87409330+TheRising3un@users.noreply.github.com>
2022-05-11 15:09:12 +0800

    test(multi-raft): add more tests about leaderElector
    1.add more tests about leaderElector

--- src/test/java/io/openmessaging/storage/dledger/LeaderElectorTest.java
+++ src/test/java/io/openmessaging/storage/dledger/LeaderElectorTest.java
@@ -235,8 +235,7 @@ public class LeaderElectorTest extends ServerTestHarness {
             }
             String followerId = server.getMemberState().getSelfId();
             dLedgerProxy.removeDLedgerServer(server);
-            System.out.println("shutdown: " + followerId);
-            dLedgerProxy.addDLedgerServer(configs.stream().filter(x -> x.getSelfId() == followerId).iterator().next(), true);
+            dLedgerProxy.addDLedgerServer(server.getdLedgerConfig(), true);
             Thread.sleep(2000);
             Assertions.assertTrue(server.getMemberState().isFollower());
             Assertions.assertTrue(leaderServer.getMemberState().isLeader());
@@ -274,7 +273,6 @@ public class LeaderElectorTest extends ServerTestHarness {
         //shutdown the leader, should elect another leader
         DLedgerProxy leaderProxy = proxies.get(leaderServer.getdLedgerConfig().getSelfId().toCharArray()[1] - '0');
         leaderProxy.shutdown();
-        System.out.println("shutdown " + leaderProxy.getConfigManager().getdLedgerProxyConfig().getConfigs().get(0).getSelfId());
         Thread.sleep(1500);
         List<DLedgerServer> leftServers = new ArrayList<>();
         for (DLedgerServer server : servers) {
@@ -324,7 +322,6 @@ public class LeaderElectorTest extends ServerTestHarness {
         Assertions.assertNotNull(leaderServer);
 
         //remove and shutdown the leader, should elect another leader
-        System.out.println("shutdown " + leaderServer.getdLedgerConfig().getSelfId());
         dLedgerProxy.removeDLedgerServer(leaderServer);
         Thread.sleep(1500);
         List<DLedgerServer> leftServers = new ArrayList<>();
@@ -375,7 +372,7 @@ public class LeaderElectorTest extends ServerTestHarness {
         Assertions.assertEquals(2, followerNum.get());
         Assertions.assertNotNull(leaderServer);
 
-        //restart the follower, the leader should keep the same
+        //shutdown the follower, the leader should keep the same
         for (DLedgerServer server : servers) {
             if (server == leaderServer) {
                 continue;
@@ -393,6 +390,52 @@ public class LeaderElectorTest extends ServerTestHarness {
         Assertions.assertEquals(term, leaderServer.getMemberState().currTerm());
     }
 
+    @Test
+    public void testThreeServerInOneProxyAndShutdownFollowers() throws Exception {
+        String group = UUID.randomUUID().toString();
+        int port = nextPort();
+        String peers = String.format("n0-localhost:%d;n1-localhost:%d;n2-localhost:%d", port, port, port);
+        List<DLedgerServer> servers = new ArrayList<>();
+        DLedgerConfig config0 = createDLedgerConfig(group, peers, "n0");
+        DLedgerConfig config1 = createDLedgerConfig(group, peers, "n1");
+        DLedgerConfig config2 = createDLedgerConfig(group, peers, "n2");
+        List<DLedgerConfig> configs = Arrays.asList(config0, config1, config2);
+        DLedgerProxyConfig dLedgerProxyConfig = new DLedgerProxyConfig();
+        dLedgerProxyConfig.setConfigs(configs);
+        DLedgerProxy dLedgerProxy = launchDLedgerProxy(dLedgerProxyConfig);
+        servers.addAll(dLedgerProxy.getDLedgerManager().getDLedgerServers());
+        AtomicInteger leaderNum = new AtomicInteger(0);
+        AtomicInteger followerNum = new AtomicInteger(0);
+
+        long start = System.currentTimeMillis();
+        while (parseServers(servers, leaderNum, followerNum) == null && DLedgerUtils.elapsed(start) < 1000) {
+            Thread.sleep(100);
+        }
+        Thread.sleep(1000);
+        leaderNum.set(0);
+        followerNum.set(0);
+
+        DLedgerServer leaderServer = parseServers(servers, leaderNum, followerNum);
+        Assertions.assertEquals(1, leaderNum.get());
+        Assertions.assertEquals(2, followerNum.get());
+        Assertions.assertNotNull(leaderServer);
+
+        //shutdown the follower, the leader should keep the same
+        for (DLedgerServer server : servers) {
+            if (server == leaderServer) {
+                continue;
+            }
+            dLedgerProxy.removeDLedgerServer(server);
+        }
+
+        long term = leaderServer.getMemberState().currTerm();
+        start = System.currentTimeMillis();
+        while (leaderServer.getMemberState().isLeader() && DLedgerUtils.elapsed(start) < 4 * leaderServer.getdLedgerConfig().getHeartBeatTimeIntervalMs()) {
+            Thread.sleep(100);
+        }
+        Assertions.assertTrue(leaderServer.getMemberState().isCandidate());
+        Assertions.assertEquals(term, leaderServer.getMemberState().currTerm());
+    }
 
     @Test
     public void testThreeServerAndPreferredLeader() throws Exception {
@@ -429,7 +472,6 @@ public class LeaderElectorTest extends ServerTestHarness {
         //1. shutdown leader.
         DLedgerProxy leaderProxy = proxies.get(leaderServer.getdLedgerConfig().getSelfId().toCharArray()[1] - '0');
         leaderProxy.shutdown();
-        System.out.println("shutdown " + leaderServer.getMemberState().getSelfId());
         Thread.sleep(1500);
         List<DLedgerServer> leftServers = new ArrayList<>();
         for (DLedgerServer server : servers) {
@@ -441,7 +483,7 @@ public class LeaderElectorTest extends ServerTestHarness {
         while (parseServers(leftServers, leaderNum, followerNum) == null && DLedgerUtils.elapsed(start) < 3 * leaderServer.getdLedgerConfig().getHeartBeatTimeIntervalMs()) {
             Thread.sleep(100);
         }
-        Thread.sleep(3000);
+        Thread.sleep(300);
         leaderNum.set(0);
         followerNum.set(0);
         leaderServer = parseServers(leftServers, leaderNum, followerNum);
@@ -456,6 +498,85 @@ public class LeaderElectorTest extends ServerTestHarness {
         DLedgerServer newPreferredNode = newPreferredProxy.getDLedgerManager().getDLedgerServers().get(0);
         leftServers.add(newPreferredNode);
 
+        leaderNum.set(0);
+        followerNum.set(0);
+
+        start = System.currentTimeMillis();
+        while (((leaderServer = parseServers(leftServers, leaderNum, followerNum)) == null || leaderServer.getMemberState().currTerm() == oldTerm)
+                && DLedgerUtils.elapsed(start) < 3000) {
+            Thread.sleep(100);
+        }
+        Thread.sleep(1500);
+        leaderNum.set(0);
+        followerNum.set(0);
+        leaderServer = parseServers(leftServers, leaderNum, followerNum);
+        Assertions.assertEquals(1, leaderNum.get());
+        Assertions.assertTrue(followerNum.get() >= 1);
+        Assertions.assertNotNull(leaderServer);
+        Assertions.assertEquals(preferredLeaderId, leaderServer.getdLedgerConfig().getSelfId());
+    }
+
+    @Test
+    public void testThreeServerInOneProxyAndPreferredLeader() throws Exception {
+        String preferredLeaderId = "n2";
+        String group = UUID.randomUUID().toString();
+        int port = nextPort();
+        String peers = String.format("n0-localhost:%d;n1-localhost:%d;n2-localhost:%d", port, port, port);
+        List<DLedgerServer> servers = new ArrayList<>();
+        DLedgerConfig config0 = createDLedgerConfig(group, peers, "n0", preferredLeaderId);
+        DLedgerConfig config1 = createDLedgerConfig(group, peers, "n1", preferredLeaderId);
+        DLedgerConfig config2 = createDLedgerConfig(group, peers, "n2", preferredLeaderId);
+        List<DLedgerConfig> configs = Arrays.asList(config0, config1, config2);
+        DLedgerProxyConfig dLedgerProxyConfig = new DLedgerProxyConfig();
+        dLedgerProxyConfig.setConfigs(configs);
+        DLedgerProxy dLedgerProxy = launchDLedgerProxy(dLedgerProxyConfig);
+        servers.addAll(dLedgerProxy.getDLedgerManager().getDLedgerServers());
+        AtomicInteger leaderNum = new AtomicInteger(0);
+        AtomicInteger followerNum = new AtomicInteger(0);
+
+        long start = System.currentTimeMillis();
+        while (parseServers(servers, leaderNum, followerNum) == null && DLedgerUtils.elapsed(start) < 1000) {
+            Thread.sleep(100);
+        }
+        Thread.sleep(3000);
+        leaderNum.set(0);
+        followerNum.set(0);
+
+        DLedgerServer leaderServer = parseServers(servers, leaderNum, followerNum);
+        Assertions.assertEquals(1, leaderNum.get());
+        Assertions.assertEquals(2, followerNum.get());
+        Assertions.assertNotNull(leaderServer);
+
+        Assertions.assertEquals(preferredLeaderId, leaderServer.getdLedgerConfig().getSelfId());
+
+        //1.remove and shutdown leader.
+        dLedgerProxy.removeDLedgerServer(leaderServer);
+        DLedgerConfig preferredLeaderConfig = leaderServer.getdLedgerConfig();
+        Thread.sleep(1500);
+        List<DLedgerServer> leftServers = new ArrayList<>();
+        for (DLedgerServer server : servers) {
+            if (server != leaderServer) {
+                leftServers.add(server);
+            }
+        }
+        start = System.currentTimeMillis();
+        while (parseServers(leftServers, leaderNum, followerNum) == null && DLedgerUtils.elapsed(start) < 3 * leaderServer.getdLedgerConfig().getHeartBeatTimeIntervalMs()) {
+            Thread.sleep(100);
+        }
+        Thread.sleep(300);
+        leaderNum.set(0);
+        followerNum.set(0);
+        leaderServer = parseServers(leftServers, leaderNum, followerNum);
+        Assertions.assertNotNull(leaderServer);
+        Assertions.assertEquals(1, leaderNum.get());
+        Assertions.assertEquals(1, followerNum.get());
+
+
+        //2. restart leader;
+        long oldTerm = leaderServer.getMemberState().currTerm();
+        dLedgerProxy.addDLedgerServer(preferredLeaderConfig, true);
+        DLedgerServer newPreferredNode = dLedgerProxy.getDLedgerManager().getDLedgerServer(preferredLeaderConfig.getSelfId());
+        leftServers.add(newPreferredNode);
         leaderNum.set(0);
         followerNum.set(0);
 
